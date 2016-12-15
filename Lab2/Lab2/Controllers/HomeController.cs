040d2b0f434f2c4b821b389eaf025760f8fdaf39
Michael <master8.michael@yahoo.com>
2016-12-15 17:39:10 +0500

    Lab2

---
+++ Lab2/Lab2/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (model.Login == "Michael" && model.Password == "123")
+            {
+                FormsAuthentication.SetAuthCookie(model.Login, false);
+
+                return RedirectToAction("Index", "Home"); ;
+            }
+
+            return View();
+        }
+    }
+}
